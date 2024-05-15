4071a592ab50e6bde85ef6214acda2e2487ed803
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-05-15 16:13:29 -0300

    trilhaapp

---
+++ trilhaapp/android/app/src/main/kotlin/com/leonardolarangeira/trilhaapp/trilhaapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.leonardolarangeira.trilhaapp.trilhaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ trilhaapp/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App  Counter'),
+      ),
+    );
+  }
+}
