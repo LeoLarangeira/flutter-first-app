60d8a7407fd9aca34c10e74f7ca9374e4daf7cbd
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-05-22 15:50:29 -0300

    hero widget

--- trilhaapp/lib/pages/card_page.dart
+++ trilhaapp/lib/pages/card_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trilhaapp/model/card_detail.dart';
+import 'package:trilhaapp/pages/card_detail.dart';
 import 'package:trilhaapp/repositories/card_detail_repository.dart';
 
 class CardPage extends StatefulWidget {
@@ -55,11 +56,75 @@ class _CardPageState extends State<CardPage> {
                   height: 10,
                 ),
                 const Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec lacinia libero. Sed a aliquet urna. Praesent eleifend diam sit amet tortor sagittis efficitur. Aliquam erat volutpat. Ut condimentum risus eget justo ultricies, id maximus arcu maximus. Praesent aliquet, augue nec ullamcorper accumsan, libero risus hendrerit nulla, nec convallis dui velit non lorem. Nam convallis tellus pellentesque sem feugiat, et mollis urna iaculis. Sed rutrum est ut mauris faucibus, luctus vestibulum risus tempus. Curabitur in lobortis lacus. Nullam molestie, lorem eget laoreet mattis, eros eros sollicitudin dolor, vel porta ligula sapien eu velit. Integer eget molestie sapien. Etiam ac vestibulum metus. Ut dignissim, leo convallis auctor mollis, mauris felis vehicula neque, non facilisis dolor ipsum at urna. ')
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec lacinia libero. Sed a aliquet urna. Praesent eleifend diam sit amet tortor sagittis efficitur. Aliquam erat volutpat. Ut condimentum risus eget justo ultricies, id maximus arcu maximus. Praesent aliquet, augue nec ullamcorper accumsan, libero risus hendrerit nulla, nec convallis dui velit non lorem. Nam convallis tellus pellentesque sem feugiat, et mollis urna iaculis. Sed rutrum est ut mauris faucibus, luctus vestibulum risus tempus. Curabitur in lobortis lacus. Nullam molestie, lorem eget laoreet mattis, eros eros sollicitudin dolor, vel porta ligula sapien eu velit. Integer eget molestie sapien. Etiam ac vestibulum metus. Ut dignissim, leo convallis auctor mollis, mauris felis vehicula neque, non facilisis dolor ipsum at urna. '),
               ],
             ),
           ),
         ),
+      ),
+      //aqui usamos o Hero
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        width: double.infinity,
+        child: cardDetail == null
+            ? LinearProgressIndicator()
+            : InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CardDetailPage(
+                                cardDetail: cardDetail!,
+                              )));
+                },
+                child: Hero(
+                  tag: cardDetail!.id,
+                  child: Card(
+                    elevation: 8,
+                    shadowColor: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Image.network(
+                                cardDetail!.url,
+                                height: 20,
+                              ),
+                              Text(
+                                cardDetail!.title,
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            cardDetail!.text,
+                            textAlign: TextAlign.justify,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.normal),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                                onPressed: () {},
+                                child: const Text("Ler Mais",
+                                    style: TextStyle(
+                                        decoration: TextDecoration.underline))),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
       ),
     ]);
   }
--- trilhaapp/lib/pages/main_page.dart
+++ trilhaapp/lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:trilhaapp/pages/card_page.dart';
+import 'package:trilhaapp/pages/list_view_h.dart';
 import 'package:trilhaapp/shared/widgets/custom_drawer.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,48 +13,45 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   PageController controller = PageController(initialPage: 0);
-  int pagePosition = 0;
+  int posicaoPagina = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'Home Page',
-            style: GoogleFonts.roboto(color: Colors.white),
-          ),
-          backgroundColor: Colors.black,
+          title: const Text("Main Page"),
         ),
         drawer: CustomDrawer(),
-        body: PageView(
+        body: Column(
           children: [
-            Column(
-              children: [
-                Expanded(
-                  child: PageView(
-                    controller: controller,
-                    onPageChanged: (value) {
-                      pagePosition = value;
-                    },
-                    children: const [
-                      CardPage(),
-                    ],
-                  ),
-                ),
-              ],
+            Expanded(
+              child: PageView(
+                controller: controller,
+                onPageChanged: (value) {
+                  setState(() {
+                    posicaoPagina = value;
+                  });
+                },
+                children: const [CardPage(), ListViewHPage()],
+              ),
             ),
             BottomNavigationBar(
-              onTap: (value) {
-                controller.jumpToPage(value);
-              },
-              currentIndex: pagePosition,
-              items: [
-                BottomNavigationBarItem(label: "Pag1", icon: Icon(Icons.home)),
-                BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.add)),
-                BottomNavigationBarItem(label: "Pag3", icon: Icon(Icons.person))
-              ],
-            )
+                type: BottomNavigationBarType.fixed,
+                onTap: (value) {
+                  controller.jumpToPage(value);
+                },
+                currentIndex: posicaoPagina,
+                items: const [
+                  BottomNavigationBarItem(
+                      label: "Pag1", icon: Icon(Icons.home)),
+                  BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.add)),
+                  BottomNavigationBarItem(
+                      label: "Pag3", icon: Icon(Icons.person)),
+                  BottomNavigationBarItem(
+                      label: "Pag4", icon: Icon(Icons.image)),
+                  BottomNavigationBarItem(
+                      label: "Tarefas", icon: Icon(Icons.list))
+                ])
           ],
         ),
       ),
